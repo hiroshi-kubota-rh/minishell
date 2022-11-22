81c91cd11fa664745e21e92a56f00e90acf01cdd
subaru nara <snara@student.42tokyo.jp>
2022-11-22 23:16:33 +0900

    add ex00

--- microshell-snara/microshell-00/ex00/main.c
+++ microshell-snara/microshell-00/ex00/main.c
@@ -1,7 +1,28 @@
 #include <stdlib.h>
-#include <stdbool.h>
+#include <string.h>
+#include <readline/readline.h>
+
+static void	microshell()
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("$> ");
+		if (!line)
+			exit(1);
+		else if (!strcmp(line, ""))
+			;
+		else if (!strcmp(line, "exit"))
+			exit(0);
+		else
+			printf("builtin not found: %s\n", line);
+		free(line);
+	}
+}
 
 int	main(void)
 {
+	microshell();
 	return (0);
 }
